4b38de8b808173bfdb6e5713b0a3915cadd7f74e
Marina Reis <marinareis.mr@gmail.com>
2023-04-30 17:50:32 -0300

    camada de servico e registro de componente

--- src/main/java/com/educandoweb/projeto/spring/resource/UserResource.java
+++ src/main/java/com/educandoweb/projeto/spring/resource/UserResource.java
@@ -1,20 +1,34 @@
 package com.educandoweb.projeto.spring.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.projeto.spring.entites.User;
+import com.educandoweb.projeto.spring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"maria","email@email.com","99889988","1234");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
 		
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> finById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
