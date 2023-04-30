afc8ad563cb111d68dcdd7b32f9b766486ed6c15
Marina Reis <marinareis.mr@gmail.com>
2023-04-30 18:11:27 -0300

    entidade order com relacionamento com user

--- src/main/java/com/educandoweb/projeto/spring/entites/User.java
+++ src/main/java/com/educandoweb/projeto/spring/entites/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.projeto.spring.entites;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,8 +28,10 @@ public class User implements Serializable { // transforma o objeto em cadeia de 
 	private String phone;
 	private String password;
 
+	@OneToMany(mappedBy = "client")//mapeado pelo atributo client de Order
+	private List<Order>orders = new ArrayList<>();//1 cliente relação com uma lista de pedido
+	
 	public User() {
-
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -76,6 +81,10 @@ public class User implements Serializable { // transforma o objeto em cadeia de 
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
