3b9409fea18d40e8d2a6517c9b14370340209245
Marina Reis <marinareis.mr@gmail.com>
2023-05-02 14:34:01 -0300

    Category

--- src/main/java/com/educandoweb/projeto/spring/config/TestConfig.java
+++ src/main/java/com/educandoweb/projeto/spring/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.projeto.spring.entites.Category;
 import com.educandoweb.projeto.spring.entites.Order;
 import com.educandoweb.projeto.spring.entites.User;
 import com.educandoweb.projeto.spring.entites.enums.OrderStatus;
+import com.educandoweb.projeto.spring.repositores.CategoryRepository;
 import com.educandoweb.projeto.spring.repositores.OrderRepository;
 import com.educandoweb.projeto.spring.repositores.UserRepository;
 
@@ -24,8 +26,17 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "1234");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "12345");
