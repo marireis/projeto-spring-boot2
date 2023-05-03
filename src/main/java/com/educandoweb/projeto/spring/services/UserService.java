c00c76cdb155dd808acab13e394bfc5a65517638
Marina Reis <marinareis.mr@gmail.com>
2023-05-03 14:29:59 -0300

    User update

--- src/main/java/com/educandoweb/projeto/spring/services/UserService.java
+++ src/main/java/com/educandoweb/projeto/spring/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.educandoweb.projeto.spring.entites.User;
 import com.educandoweb.projeto.spring.repositores.UserRepository;
@@ -30,5 +31,18 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);//monitora o obje no jpa para mexer
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {//metodo para atualizar o dados do entity
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
 	}
 }
